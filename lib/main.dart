d9862465702c128fce39cbae01796975796a5553
mohaddese sandoghdar <m.sandoghdar6927@gmail.com>
2024-03-23 00:17:53 +0330

    feat: sotory list

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:blogclub/data.dart';
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -20,14 +21,20 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
           textTheme: const TextTheme(
-              titleMedium: TextStyle(
-                  fontFamily: defaultFontFamily,
-                  fontSize: 14,
-                  color: secondaryTextColor),
-              titleLarge: TextStyle(
-                  fontFamily: defaultFontFamily,
-                  fontWeight: FontWeight.bold,
-                  color: primaryTextColor))),
+            titleMedium: TextStyle(
+                fontFamily: defaultFontFamily,
+                fontSize: 14,
+                color: secondaryTextColor),
+            titleLarge: TextStyle(
+                fontFamily: defaultFontFamily,
+                fontWeight: FontWeight.bold,
+                color: primaryTextColor),
+            headlineMedium: TextStyle(
+                fontFamily: defaultFontFamily,
+                fontWeight: FontWeight.w700,
+                fontSize: 24,
+                color: primaryTextColor),
+          )),
       debugShowCheckedModeBanner: false,
       home: const MyHomePage(),
     );
@@ -66,73 +73,130 @@ class MyHomePage extends StatelessWidget {
                 ),
               ),
               Padding(
-                padding: const EdgeInsets.fromLTRB(32, 0, 32, 0),
+                padding: const EdgeInsets.fromLTRB(32, 0, 0, 0),
                 child: Text(
                   'Explore Today’s',
-                  style: themeData.textTheme.titleLarge,
+                  style: themeData.textTheme.headlineMedium,
                 ),
               ),
-              Container(
-                width: MediaQuery.of(context).size.width,
-                height: 90,
-                margin: const EdgeInsets.fromLTRB(32, 10, 0, 0),
-                child: ListView.builder(
-                    itemCount: AppDatabase.stories.length,
-                    scrollDirection: Axis.horizontal,
-                    // padding:
-                    itemBuilder: (context, index) {
-                      return Column(
-                        children: [
-                          Stack(
-                            children: [
-                              Container(
-                                width: 68,
-                                height: 68,
-                                margin: const EdgeInsets.only(right: 8),
-                                padding: const EdgeInsets.all(3),
-                                decoration: BoxDecoration(
-                                  gradient: const LinearGradient(
-                                      colors: [
-                                        Color(0xff376AED),
-                                        Color(0xff49B0E2),
-                                        Color(0xff9CECFB),
-                                      ],
-                                      begin: Alignment.topLeft,
-                                      end: Alignment.bottomRight),
-                                  borderRadius: BorderRadius.circular(24),
-                                ),
-                                child: Container(
-                                  decoration: BoxDecoration(
-                                    color: Colors.white,
-                                    borderRadius: BorderRadius.circular(20),
-                                  ),
-                                  padding: const EdgeInsets.all(4),
-                                  child: ClipRRect(
-                                    borderRadius: BorderRadius.circular(20),
-                                    child: Image.asset(
-                                        'assets/img/stories/${AppDatabase.stories[index].imageFileName}'),
-                                  ),
-                                ),
-                              ),
-                              Positioned(
-                                  right: 0,
-                                  bottom: 0,
-                                  child: Image.asset(
-                                    'assets/img/icons/${stories[index].iconFileName}',
-                                    width: 24,
-                                    height: 24,
-                                  ))
-                            ],
-                          ),
-                          const SizedBox(
-                            height: 2,
-                          ),
-                          Text(AppDatabase.stories[index].name)
-                        ],
-                      );
-                    }),
-              ),
+              _StoryList(stories: stories),
             ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _StoryList extends StatelessWidget {
+  const _StoryList({
+    required this.stories,
+  });
+
+  final List<Story> stories;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: 90,
+      margin: const EdgeInsets.fromLTRB(32, 16, 0, 0),
+      child: ListView.builder(
+          itemCount: AppDatabase.stories.length,
+          scrollDirection: Axis.horizontal,
+          physics: const BouncingScrollPhysics(),
+          itemBuilder: (context, index) {
+            return _Story(story: stories[index]);
+          }),
+    );
+  }
+}
+
+class _Story extends StatelessWidget {
+  const _Story({
+    required this.story,
+  });
+
+  final Story story;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Stack(
+          children: [
+            story.isViewed ? viewedStrory() : normalStory(),
+            Positioned(
+                right: 0,
+                bottom: 0,
+                child: Image.asset(
+                  'assets/img/icons/${story.iconFileName}',
+                  width: 24,
+                  height: 24,
+                ))
+          ],
+        ),
+        const SizedBox(
+          height: 2,
+        ),
+        Text(story.name)
+      ],
+    );
+  }
+
+  Widget normalStory() {
+    return Container(
+      width: 68,
+      height: 68,
+      margin: const EdgeInsets.only(right: 8),
+      padding: const EdgeInsets.all(2),
+      decoration: BoxDecoration(
+        gradient: const LinearGradient(colors: [
+          Color(0xff376AED),
+          Color(0xff49B0E2),
+          Color(0xff9CECFB),
+        ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+        borderRadius: BorderRadius.circular(24),
+      ),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(22),
+        ),
+        padding: const EdgeInsets.all(4),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(17),
+          child: Image.asset('assets/img/stories/${story.imageFileName}'),
+        ),
+      ),
+    );
+  }
+
+  Widget viewedStrory() {
+    return Container(
+      width: 68,
+      height: 68,
+      margin: const EdgeInsets.only(right: 8),
+      padding: const EdgeInsets.all(2),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(24),
+      ),
+      child: DottedBorder(
+        padding: EdgeInsets.zero,
+        radius: const Radius.circular(22),
+        color: const Color(0xff7B8BB2),
+        strokeWidth: 2,
+        dashPattern: const [8, 3],
+        borderType: BorderType.RRect,
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(22),
+          ),
+          padding: const EdgeInsets.all(4),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(17),
+            child: Image.asset('assets/img/stories/${story.imageFileName}'),
           ),
         ),
       ),
